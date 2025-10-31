7d0a6324aec82fa5a86a9b8d6edf3bdc584ea784
Eva <molinereva00@gmail.com>
2025-10-31 07:49:10 +0100

    Delete Problem Set 0/Cyberchase/emoliga-d8c34e59968c69f162390cb97a4138056d40c8c2 directory

--- Problem Set 0/Cyberchase/emoliga-d8c34e59968c69f162390cb97a4138056d40c8c2/11.sql
+++
@@ -1,4 +0,0 @@
--- list the titles of episodes from season 5, in reverse alphabetical order.
-SELECT "title" FROM "episodes"
-WHERE "season" = 5
-ORDER BY "title" DESC;
--- Problem Set 0/Cyberchase/emoliga-d8c34e59968c69f162390cb97a4138056d40c8c2/13.sql
+++
@@ -1,8 +0,0 @@
--- write a SQL query to explore a question of your choice. This query should: Involve at least one condition, using WHERE with AND or OR
-
--- this query will list the season, title and topic of the episodes not from season 4 to 6 (included) and which topic is not null ordered
--- first descendent by season and then ascendent by title
-
-SELECT "season", "title", "topic" FROM "episodes"
-WHERE ("season" <= 3 OR "season" >= 7) AND "topic" NOT NULL
-ORDER BY "season" DESC, "title";
