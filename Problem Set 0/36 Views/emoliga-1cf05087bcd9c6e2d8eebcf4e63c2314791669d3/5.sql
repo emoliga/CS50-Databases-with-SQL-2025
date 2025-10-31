a7f816ae8762fe5349656405de7170de54747086
Eva <molinereva00@gmail.com>
2025-10-31 07:49:01 +0100

    Delete Problem Set 0/36 Views/emoliga-1cf05087bcd9c6e2d8eebcf4e63c2314791669d3 directory

--- Problem Set 0/36 Views/emoliga-1cf05087bcd9c6e2d8eebcf4e63c2314791669d3/5.sql
+++
@@ -1,6 +0,0 @@
--- write a SQL query to find the highest contrast value of prints by Hokusai. Name the column “Maximum Contrast”. Does Hokusai’s prints most contrasting
--- print actually have much contrast?
-
-SELECT MAX ( "contrast" ) FROM "views"
-AS "Maximum Contrast"
-WHERE "artist" = "Hokusai";
